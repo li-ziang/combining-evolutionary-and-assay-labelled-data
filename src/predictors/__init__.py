a1ac0e9783ebe9500088e135951ea652e87f6e29
Ziang Li <ziang.li2001@gmail.com>
2024-09-04 11:14:04 -0400

    esm+ddg

--- src/predictors/__init__.py
+++ src/predictors/__init__.py
@@ -17,9 +17,22 @@ BASIC_PREDICTOR_MAP = {
     'gesm': esm_predictors.GlobalESMPredictor,
     'gesm_reg': esm_predictors.GlobalESMRegressionPredictor,
     'vae': vae_predictors.VaePredictor,
+    'ddg': esm_predictors.DDGPredictor,
 }
 
 CORE_PREDICTORS = [
+    # 'onehot',
+    # 'ev+onehot', 
+    'gesm+onehot', 
+    # 'eunirep_ll+onehot',
+    # 'vae+onehot', 
+    # 'ev+onehot+ddg', 
+    'gesm+onehot+ddg', 
+    # 'eunirep_ll+onehot+ddg',
+    # 'vae+onehot+ddg'
+]
+
+OLD_CORE_PREDICTORS = [
     'eunirep_reg', 'ev+onehot', 'gesm+onehot', 'eunirep_ll+onehot',
     'vae+onehot',
 ]
